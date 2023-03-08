4ba4f39baff3329a4d51d5e09fb70e0d65a4fc4b
Enes CAN <enesscann98@gmail.com>
2023-03-08 10:01:49 +0300

    Every frame is saved as soon as the preview view is opened.

--- app/src/main/java/com/candroid/learningcamera/MainActivity.kt
+++ app/src/main/java/com/candroid/learningcamera/MainActivity.kt
@@ -3,66 +3,116 @@ package com.candroid.learningcamera
 import android.content.pm.PackageManager
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.util.Log
-import android.view.View
-import android.widget.Toast
 import androidx.camera.core.*
 import androidx.camera.lifecycle.ProcessCameraProvider
 import androidx.core.app.ActivityCompat
 import androidx.core.content.ContextCompat
 import com.candroid.learningcamera.databinding.ActivityMainBinding
-import com.google.common.util.concurrent.ListenableFuture
+import java.io.File
+import java.nio.ByteBuffer
+import java.text.SimpleDateFormat
+import java.util.*
 import java.util.concurrent.ExecutorService
+import java.util.concurrent.Executors
 
 class MainActivity : AppCompatActivity() {
     private lateinit var binding: ActivityMainBinding
+    private var imageCapture: ImageCapture? = null
+    private lateinit var outputDirectory: File
+    private lateinit var cameraExecutor: ExecutorService
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
-        binding.btnOpenCamera.setOnClickListener {
-            if (cameraPermissionGranted()){
-                startCamera()
-            } else{
-                Toast.makeText(baseContext, "We do not have permission", Toast.LENGTH_LONG).show()
-                ActivityCompat.requestPermissions(this, arrayOf(android.Manifest.permission.CAMERA), 1)
-            }
+        outputDirectory = getOutputDirectory()
+        cameraExecutor = Executors.newSingleThreadExecutor()
+
+        if (cameraPermissionGranted()){
+            startCamera()
+        } else{
+            ActivityCompat.requestPermissions(this, arrayOf(android.Manifest.permission.CAMERA), 1)
         }
     }
 
-    private fun cameraPermissionGranted() = ContextCompat.checkSelfPermission(baseContext, android.Manifest.permission.CAMERA) == PackageManager.PERMISSION_GRANTED
+    private fun cameraPermissionGranted() = ContextCompat.checkSelfPermission(
+        baseContext,
+        android.Manifest.permission.CAMERA
+    ) == PackageManager.PERMISSION_GRANTED
 
     private fun startCamera() {
         val cameraProviderFuture = ProcessCameraProvider.getInstance(this)
 
-        cameraProviderFuture.addListener({
-            // Used to bind the lifecycle of cameras to the lifecycle owner
+        cameraProviderFuture.addListener(Runnable {
             val cameraProvider: ProcessCameraProvider = cameraProviderFuture.get()
 
-            // Preview
             val preview = Preview.Builder()
                 .build()
                 .also {
                     it.setSurfaceProvider(binding.viewFinder.surfaceProvider)
                 }
 
-            // Select back camera as a default
-            val cameraSelector = CameraSelector.DEFAULT_BACK_CAMERA
+            imageCapture = ImageCapture.Builder()
+                .build()
+
+            val imageAnalysis = ImageAnalysis.Builder()
+                .build()
+                .also {
+                    it.setAnalyzer(cameraExecutor, FrameAnalyzer())
+                }
+
+            val cameraSelector = CameraSelector.Builder()
+                .requireLensFacing(CameraSelector.LENS_FACING_BACK)
+                .build()
 
             try {
-                // Unbind use cases before rebinding
                 cameraProvider.unbindAll()
 
-                // Bind use cases to camera
                 cameraProvider.bindToLifecycle(
-                    this, cameraSelector, preview)
+                    this, cameraSelector, preview, imageCapture, imageAnalysis
+                )
 
-            } catch(exc: Exception) {
-                Log.e("CameraX", "Use case binding failed", exc)
+            } catch (e: Exception) {
+                e.printStackTrace()
             }
 
         }, ContextCompat.getMainExecutor(this))
     }
+
+    private inner class FrameAnalyzer : ImageAnalysis.Analyzer {
+        override fun analyze(image: ImageProxy) {
+            // Save every frame taken as soon as the PreviewView is opened
+            saveImage(image)
+
+            image.close()
+        }
+    }
+
+    private fun saveImage(image: ImageProxy) {
+        val buffer: ByteBuffer = image.planes[0].buffer
+        val bytes = ByteArray(buffer.remaining())
+        buffer.get(bytes)
+
+        val file = File(
+            outputDirectory,
+            "IMG_${
+                SimpleDateFormat(
+                    "yyyyMMdd_HHmmss_SSS",
+                    Locale.getDefault()
+                ).format(System.currentTimeMillis())
+            }.jpg"
+        )
+
+        file.writeBytes(bytes)
+    }
+
+    private fun getOutputDirectory(): File {
+        val mediaDir = externalMediaDirs.firstOrNull()?.let {
+            File(it, resources.getString(R.string.app_name)).apply { mkdirs() }
+        }
+        return if (mediaDir != null && mediaDir.exists())
+            mediaDir else filesDir
+    }
+
 }
